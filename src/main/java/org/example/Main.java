1443837cdcfb05f8097e4bc535e32214fa2a6e39
Tomáš Dvořák <tomassdvorakk@gmail.com>
2025-10-02 08:10:04 +0200

    .

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,45 +7,24 @@ import org.example.bankAccounts.services.BankAccountService;
 import org.example.people.BasePerson;
 import org.example.people.NormalPerson;
 import org.example.people.StudentPerson;
+import org.example.customers.factories.CustomerFactory;
+import org.example.customers.Customer;
 
 public class Main {
     public static void main(String[] args) {
 
         BankAccountNumberGenerator generator = new BankAccountNumberGenerator(); //Tady zakládám instanci třídy BankAccountNumberGenerator
         BankAccountService service = new BankAccountService();
+        CustomerFactory customerFactory = new CustomerFactory();
 
-        //Vytvoření normální osoby a spořicího účtu
-        BasePerson tomas = new BasePerson("p1", "Tomáš", "Dvořák");
-        //SavingBankAccount tomasAccount = new SavingBankAccount("a1", 1000, generator.generateRandomAccountNumber(), tomas, 0.05);
+        BasePerson person = new BasePerson(
+                "1",
+                "John",
+                "Doe"
+        );
 
+        Customer owner = customerFactory.createCustomer("1", person.getFirstName(), person.getLastName());
 
-        //NormalPerson normal = new NormalPerson("p2", "Jan", "Novák", tomasAccount);
-
-        //Výpis normálního člověka
-        //System.out.println("Normal Person: " + normal.getFirstName() + " " + normal.getLastName());
-        //System.out.println("Balance: " + tomasAccount.getBalance());
-        //System.out.println("Account number: " + tomasAccount.getBankAccountNumber());
-
-        //Přidání zůstatku
-        //service.deposit(tomasAccount, 500);
-        //System.out.println("New balance: " + tomasAccount.getBalance());
-
-        //Odebrání zůstatku
-        //service.withdraw(tomasAccount, 1400);
-        //System.out.println("New balance: " + tomasAccount.getBalance());
-
-
-
-
-
-        //Vytvoření studenta a studentského účtu
-        StudentPerson student = new StudentPerson("p3", "Petr", "Svoboda", "Delta");
-        //StudentBankAccount studentAccount = new StudentBankAccount("a2", 500, generator.generateRandomAccountNumber(), student);
-
-        System.out.println("\nStudent Person: " + student.getFirstName() + " " + student.getLastName());
-        //System.out.println("School: " + studentAccount.getSchoolName());
-        //System.out.println("Balance: " + studentAccount.getBalance());
-        //System.out.println("Account number: " + studentAccount.getBankAccountNumber());
 
     }
 }
--- src/main/java/org/example/bankAccounts/services/AML.java
+++ src/main/java/org/example/bankAccounts/services/AML.java
@@ -1,8 +1,12 @@
 package org.example.bankAccounts.services;
 
 public class AML {
-    public void checkAML()
-    {
 
+    public boolean amlCheck(double amount) {
+        if (amount > 10000)
+        {
+            throw new IllegalArgumentException("Transaction flagged for AML review.");
+        }
+        return true;
     }
 }
