2abfb1702f7999390e243f965e5ab6fcdbe8ba45
Tomáš Dvořák <tomassdvorakk@gmail.com>
2025-10-02 10:46:56 +0200

    .

--- src/main/java/org/example/bankAccounts/services/BankAccountService.java
+++ src/main/java/org/example/bankAccounts/services/BankAccountService.java
@@ -1,12 +1,15 @@
 package org.example.bankAccounts.services;
 
 import org.example.bankAccounts.BaseBankAccount;
+import org.example.bankAccounts.services.AML;
 
 public class BankAccountService {
 
     public void deposit(BaseBankAccount account, int amount)
     {
-        if(amount <= 0)
+        AML aml = new AML();
+
+        if(amount <= 0 && aml.amlCheck(amount))
         {
             throw new  IllegalArgumentException("Amount must be greater than 0.");
         }
