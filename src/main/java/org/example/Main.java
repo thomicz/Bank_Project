0603dfec5a2cf27b05cfc6d37beddcd857213711
Tomáš Dvořák <tomassdvorakk@gmail.com>
2025-10-16 11:23:18 +0200

    .

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.example.accounts.factories.BankAccountFactory;
 import org.example.accounts.generators.BankAccountNumberGenerator;
 import org.example.cards.PaymentCard;
 import org.example.cards.factories.PaymentCardFactory;
@@ -13,6 +14,7 @@ public class Main {
 
         BankAccountNumberGenerator generator = new BankAccountNumberGenerator();
         CustomerFactory customerFactory = new CustomerFactory();
+        BankAccountFactory bankAccountFactory = new BankAccountFactory();
         PaymentCardFactory paymentCardFactory = new PaymentCardFactory();
         BankAccountWithPaymentCardsService bankAccountWithPaymentCardsService = new BankAccountWithPaymentCardsService();
 
@@ -21,8 +23,8 @@ public class Main {
         Customer customer_2 = customerFactory.createCustomer(generator.generateRandomAccountNumber(), "Honza", "Dvořák");
 
         //Vytvoření dvou platebních účtů
-        BankAccountWithPaymentCards account_1 = new BankAccountWithPaymentCards("uuid1", "123-456", customer_1, 5000);
-        BankAccountWithPaymentCards account_2 = new BankAccountWithPaymentCards("uuid2", "987-654", customer_2, 3000);
+        BankAccountWithPaymentCards account_1 = new BankAccountWithPaymentCards("uuid1", "45678764567/0100", customer_1, 5000);
+        BankAccountWithPaymentCards account_2 = new BankAccountWithPaymentCards("uuid2", "98765456566/0100", customer_2, 3000);
 
         //Vytvoření 4 platebních karet
         PaymentCard card_1 = paymentCardFactory.create();
@@ -45,33 +47,21 @@ public class Main {
         //Tady zkouším, jestli funguje placení
         System.out.println("---------------------------------------------------------------------");
         System.out.println("Na 1. uctu je: " + account_1.getBalance() + "Kc");
-        System.out.println("---------------------------------------------------------------------");
         bankAccountWithPaymentCardsService.pay(card_1.getCardNumber(), 500);
         System.out.println("Na 1. uctu je: " + account_1.getBalance() + "Kc");
         System.out.println("---------------------------------------------------------------------");
+        System.out.println("Na 1. uctu je: " + account_1.getBalance() + "Kc");
         bankAccountWithPaymentCardsService.pay(card_2.getCardNumber(), 1000);
         System.out.println("Na 1. uctu je: " + account_1.getBalance() + "Kc");
         System.out.println("---------------------------------------------------------------------");
         System.out.println("Na 2. uctu je: " + account_2.getBalance() + "Kc");
-        System.out.println("---------------------------------------------------------------------");
         bankAccountWithPaymentCardsService.pay(card_3.getCardNumber(), 500);
         System.out.println("Na 2. uctu je: " + account_2.getBalance() + "Kc");
         System.out.println("---------------------------------------------------------------------");
+        System.out.println("Na 2. uctu je: " + account_2.getBalance() + "Kc");
         bankAccountWithPaymentCardsService.pay(card_4.getCardNumber(), 1000);
         System.out.println("Na 2. uctu je: " + account_2.getBalance() + "Kc");
         System.out.println("---------------------------------------------------------------------");
-
-
-
-
-
-
-
-
-
-
-
-
 
     }
 }
